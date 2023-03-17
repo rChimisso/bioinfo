ef84e8c339e67db47e1c44d7ea720ade5c804080
Crystal Spider <riccardo.chimisso@gmail.com>
2023-03-17 10:39:39 +0100

    Aggiornate funzioni di costruzione dei k-meri.

--- kmers.py
+++ kmers.py
@@ -2,25 +2,19 @@ from multiprocessing import Pool, cpu_count
 from Bio.SeqIO.QualityIO import FastqPhredIterator
 from Bio.Seq import Seq
 
-def build_k_mers(reads: FastqPhredIterator, k: int) -> list[Seq]:
+def build_k_mers(reads: FastqPhredIterator, k: int) -> list[list[Seq]]:
   """
-  Builds and returns all k-mers from the given list of reads.
-  Instead of a list of k-mers for each read, the returned list is flattened.
-  Runs in series.
+  Builds and returns all k-mers from the given reads.
 
   :param reads: Iterator of reads.
   :type reads: FastqPhredIterator
   :param k: Length of the k-mers. All reads shorter than k are discarted.
   :type k: int
 
-  :return: Flattened list of all k-mers for each read.
+  :return: List of lists, k-mers for each read.
   :rtype: list[Seq]
   """
-  seqs: list[Seq] = [read.seq for read in reads if len(read) >= k]
-  print(f'Number of reads that can be used to build the k-mers: {len(seqs)}')
-  k_mers = [[seq[i : i + k] for i in range(len(seq) - k + 1)] for seq in seqs]
-  print('Terminated building all k-mers, proceeding with flattening.')
-  return [k_mer for seq_k_mers in k_mers for k_mer in seq_k_mers]
+  return [[seq[i : i + k] for i in range(len(seq) - k + 1)] for read in reads if len((seq := read.seq)) >= k]
 
 def build_k_mers_helper(args: tuple[Seq, int]) -> list[Seq]:
   """
@@ -37,10 +31,9 @@ def build_k_mers_helper(args: tuple[Seq, int]) -> list[Seq]:
   k_mers = [seq[i : i + k] for i in range(len(seq) - k + 1)]
   return k_mers
 
-def build_k_mers_parallel(reads: FastqPhredIterator, k: int) -> list[Seq]:
+def build_k_mers_parallel(reads: FastqPhredIterator, k: int) -> list[list[Seq]]:
   """
-  Builds and returns all k-mers from the given list of reads.
-  Instead of a list of k-mers for each read, the returned list is flattened.
+  Builds and returns all k-mers from the given reads.
   Runs in parallel.
 
   :param reads: Iterator of reads.
@@ -48,12 +41,9 @@ def build_k_mers_parallel(reads: FastqPhredIterator, k: int) -> list[Seq]:
   :param k: Length of the k-mers. All reads shorter than k are discarted.
   :type k: int
 
-  :return: Flattened list of all k-mers for each read.
+  :return: List of lists, k-mers for each read.
   :rtype: list[Seq]
   """
-  seqs: list[Seq] = [read.seq for read in reads if len(read) >= k]
-  print(f'Number of reads that can be used to build the k-mers: {len(seqs)}')
   with Pool(cpu_count()) as pool:
-    k_mers = pool.map(build_k_mers_helper, [(seq, k) for seq in seqs])
-  print('Terminated building all k-mers, proceeding with flattening.')
-  return [k_mer for seq_k_mers in k_mers for k_mer in seq_k_mers]
+    k_mers = pool.map(build_k_mers_helper, [(seq, k) for read in reads if len((seq := read.seq)) >= k])
+  return k_mers
